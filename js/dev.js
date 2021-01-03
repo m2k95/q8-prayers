e4bc7057f09ccff8a31775aab3f86f92b43a7c3a
mymk95 <m.filkawi@gmail.com>
2021-01-03 21:35:45 +0300

    js dev compiled

--- js/dev.js
+++ js/dev.js
@@ -119,6 +119,12 @@ fetchPrayers(today, lang).then(prayers =>{
   
     const title_tr = document.createElement('tr')
     const title_th = document.createElement('th')
+    const title_small = document.createElement('small')
+
+    const credits_tr = document.createElement('tr')
+    const credits_th = document.createElement('th')
+    const credits_small = document.createElement('small')
+    const credits_a = document.createElement('a')
 
     const date_tr = document.createElement('tr')
     const date_th = document.createElement('th')
@@ -144,8 +150,17 @@ fetchPrayers(today, lang).then(prayers =>{
     const isha_td = document.createElement('td')
   
     title_th.setAttribute('colspan', '2')
-    title_th.innerHTML = prayers.Title
+    title_small.innerHTML = prayers.Title
+    title_th.appendChild(title_small)
     title_tr.appendChild(title_th)
+  
+    credits_th.setAttribute('colspan', '2')
+    credits_a.setAttribute('href', 'https://github.com/mymk95/q8-prayers')
+    credits_a.setAttribute('target', '_blank')
+    credits_a.innerHTML = 'Q8 Prayer Times'
+    credits_small.appendChild(credits_a)
+    credits_th.appendChild(credits_small)
+    credits_tr.appendChild(credits_th)
 
     date_th.setAttribute('colspan', '2')
     date_th.innerHTML = prayers.Date
@@ -183,6 +198,7 @@ fetchPrayers(today, lang).then(prayers =>{
     table.appendChild(asr_tr)
     table.appendChild(maghrib_tr)
     table.appendChild(isha_tr)
+    table.appendChild(credits_tr)
   
     table.style.width = width + 'px'
   
