7c880024c72d76b84ea8772a3340f1f02d942c5f
mymk95 <m.filkawi@gmail.com>
2021-01-19 23:31:43 +0300

    css injection support

---
+++ js/import-css.js
@@ -0,0 +1,6 @@
+var head  = document.getElementsByTagName('head')[0]
+var link  = document.createElement('link')
+link.rel  = 'stylesheet'
+link.type = 'text/css'
+link.href = 'https://cdn.jsdelivr.net/gh/mymk95/q8-prayers@1.0.6/dist/all.min.css'
+head.appendChild(link)
