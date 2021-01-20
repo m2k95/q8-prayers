0df15bccfc102b2d2bf62312f66664aa8d708a74
mymk95 <m.filkawi@gmail.com>
2021-01-20 16:37:28 +0300

    v1.0.8 build

--- js/import-css.js
+++ js/import-css.js
@@ -2,5 +2,5 @@ var head  = document.getElementsByTagName('head')[0]
 var link  = document.createElement('link')
 link.rel  = 'stylesheet'
 link.type = 'text/css'
-link.href = 'https://cdn.jsdelivr.net/gh/mymk95/q8-prayers@1.0.7/dist/all.min.css'
+link.href = 'https://cdn.jsdelivr.net/gh/mymk95/q8-prayers@1.0.8/dist/all.min.css'
 head.appendChild(link)
