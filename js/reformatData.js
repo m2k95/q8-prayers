600263f199227246c8f6ae8fb6faa1b998859059
mymk95 <m.filkawi@gmail.com>
2021-02-06 19:48:41 +0300

    ampm fix

--- js/reformatData.js
+++ js/reformatData.js
@@ -2,10 +2,11 @@ function reformatDate(lang, date) {
   const h = date.toString().split(':')[0]
   const m = date.toString().split(':')[1]
 
-  var am, pm
+  var am, pm, ampm
 
   lang === 'ar' ? am = 'ص' : am = 'AM'
   lang === 'ar' ? pm = 'م' : pm = 'PM'
+  ampm = parseInt(h) >= 12 ? pm : am
 
-  return parseInt(h) > 12 ? `${parseInt(h) - 12}:${m} ${pm}` : `${parseInt(h)}:${m} ${am}`
+  return parseInt(h) > 12 ? `${parseInt(h) - 12}:${m} ${ampm}` : `${parseInt(h)}:${m} ${ampm}`
 }
