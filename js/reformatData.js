ef891cb70f7bfc644112aee8b3aae957cd888008
myk <m2kio@pm.me>
2021-12-31 22:26:41 +0300

    reformat date function removed

--- js/reformatData.js
+++
@@ -1,12 +0,0 @@
-function reformatDate(lang, date) {
-  const h = date.toString().split(':')[0]
-  const m = date.toString().split(':')[1]
-
-  var am, pm, ampm
-
-  lang === 'ar' ? am = 'ص' : am = 'AM'
-  lang === 'ar' ? pm = 'م' : pm = 'PM'
-  ampm = parseInt(h) >= 12 ? pm : am
-
-  return parseInt(h) > 12 ? `${parseInt(h) - 12}:${m} ${ampm}` : `${parseInt(h)}:${m} ${ampm}`
-}
