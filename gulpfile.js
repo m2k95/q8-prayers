47dfaa6d1e6c106d0a580d8a17d766cafb0c2931
myk <m2kio@pm.me>
2021-12-31 22:28:07 +0300

    dev & prod tasks separated

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var uglifycss = require('gulp-uglifycss');
-var rename = require("gulp-rename");
-var concat = require('gulp-concat');
-var ignore = require('gulp-ignore');
-var gutil = require('gulp-util');
-var minifyJS = require('gulp-minify');
-var header = require("gulp-header");
-var pkg = require("./package.json");
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var uglifycss = require('gulp-uglifycss')
+var rename = require("gulp-rename")
+var concat = require('gulp-concat')
+var ignore = require('gulp-ignore')
+var gutil = require('gulp-util')
+var minifyJS = require('gulp-minify')
+var header = require("gulp-header")
+var pkg = require("./package.json")
 
 var banner = ["/**",
     " * <%= pkg.name %> v<%= pkg.version %>",
@@ -18,40 +18,27 @@ var banner = ["/**",
 	" * Author <%= pkg.author.name %> <<%= pkg.author.email %>>",
 	" * License <%= pkg.license %>",
 	" */",
-  ""].join("\n");
+  ""].join("\n")
 
-var scripts = [
-  './js/toArabicNumbers.js',
-  './js/reformatData.js',
-  './js/fetchPrayers.js',
-  './js/app.js',
-]
-
-var scripts_dev = [
-  './js/toArabicNumbers.js',
-  './js/reformatData.js',
-  './js/fetchPrayers.js',
-  './js/app.js',
-]
-
-gulp.task('sass', async function(){
-  gulp.src('./scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(uglifycss())
-    .pipe(rename("all.min.css"))
-    .pipe(header(banner, {pkg: pkg}))
-    .pipe(gulp.dest('./dist'));
-});
+// development tasks
+gulp.task('scripts-dev', async function(){
+  gulp.src('./js/**/*.js')
+  .pipe(concat('dev.js'))
+  .pipe(ignore.exclude(['**/*.map']))
+  .on('error', function(err){ gutil.log(gutil.colors.red('[Error]'), err.toString()) })
+  .pipe(gulp.dest('./js'))
+})
 
 gulp.task('sass-dev', async function(){
-  gulp.src('./scss/*.scss')
+  gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(rename("dev.css"))
-    .pipe(gulp.dest('./scss'));
-});
+    .pipe(gulp.dest('./scss'))
+})
 
+// production tasks
 gulp.task('scripts', async function(){
-  gulp.src(scripts)
+  gulp.src('./js/**/*.js')
   .pipe(concat('all.js'))
   .pipe(ignore.exclude(['**/*.map']))
   .pipe(minifyJS({
@@ -60,25 +47,27 @@ gulp.task('scripts', async function(){
     },
     noSource: true
   }))
-  .on('error', function(err){ gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+  .on('error', function(err){ gutil.log(gutil.colors.red('[Error]'), err.toString()) })
   .pipe(header(banner, {pkg: pkg}))
   .pipe(gulp.dest('./dist'))
-});
-
-gulp.task('scripts-dev', async function(){
-  gulp.src(scripts_dev)
-  .pipe(concat('dev.js'))
-  .pipe(ignore.exclude(['**/*.map']))
-  .on('error', function(err){ gutil.log(gutil.colors.red('[Error]'), err.toString()); })
-  .pipe(gulp.dest('./js'))
-});
-
-gulp.task('run-dev', gulp.series('sass-dev', 'scripts-dev'));
-
-gulp.task('watch', function(){
-  gulp.watch('./scss/*.scss', gulp.series('sass-dev'));
-  gulp.watch(scripts_dev, gulp.series('scripts-dev'));
 })
 
-gulp.task('build', gulp.series('sass', 'scripts'));
-gulp.task('default', gulp.series('run-dev', 'watch'));
+gulp.task('sass', async function(){
+  gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(uglifycss())
+    .pipe(rename("all.min.css"))
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(gulp.dest('./dist'))
+})
+
+// tasks
+gulp.task('run-dev', gulp.series('sass-dev', 'scripts-dev'))
+
+gulp.task('watch', function(){
+  gulp.watch('./scss/**/*.scss', gulp.series('sass-dev'))
+  gulp.watch('./js/**/*.js', gulp.series('scripts-dev'))
+})
+
+gulp.task('build', gulp.series('sass', 'scripts'))
+gulp.task('default', gulp.series('run-dev', 'watch'))
